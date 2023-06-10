428994baf8d9bd50d580d24564b6ae6b0bd1bc5a
Herdi Bintang <herdibintang@gmail.com>
2023-06-10 11:20:24 +0700

    add clock out method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   def clock_in(time)
     self.sleeps.create!(clock_in: time)
   end
+
+  def clock_out(time)
+    self.sleeps.create!(clock_out: time)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe User, type: :model do
     user = User.create!
 
     time = "2023-06-20 21:59:59"
-    
+
     user.clock_in(time)
 
     expect(user.sleeps[0].clock_in.strftime("%F %T")).to eq(time)
+  end
+
+  it "can do clock out" do
+    user = User.create!
+
+    time = "2023-06-20 21:59:59"
+
+    user.clock_out(time)
+
+    expect(user.sleeps[0].clock_out.strftime("%F %T")).to eq(time)
   end
 end
