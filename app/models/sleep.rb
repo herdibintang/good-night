d5798c49638273d958bf320f08642ff7d432909a
Herdi Bintang <herdibintang@gmail.com>
2023-06-11 17:10:44 +0700

    clock out cannot be less than clock in

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -1,4 +1,6 @@
 class Sleep < ApplicationRecord
+  validate :clock_out_cannot_be_less_then_clock_in
+
   belongs_to :user
 
   def clock_out=(value)
@@ -8,4 +10,13 @@ class Sleep < ApplicationRecord
       self.duration_in_second = self.clock_out - self.clock_in
     end
   end
+
+  private
+
+  def clock_out_cannot_be_less_then_clock_in
+    if clock_out.present? && clock_out < clock_in
+      errors.add(:clock_out, "cannot be less than clock in")
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,71 +7,89 @@ RSpec.describe User, type: :model do
     expect(user).not_to be_valid
   end
 
-  it "can do clock in" do
-    user = User.create!(name: "Alice")
+  context "clock in" do
+    it "can do clock in" do
+      user = User.create!(name: "Alice")
 
-    time = "2023-06-20 21:59:59"
+      time = "2023-06-20 21:59:59"
 
-    user.clock_in(time)
+      user.clock_in(time)
 
-    expect(user.sleeps[0].clock_in.strftime("%F %T")).to eq(time)
+      expect(user.sleeps[0].clock_in.strftime("%F %T")).to eq(time)
+    end
+
+    it "cannot do clock in if there is a clock in without clock out" do
+      user = User.create!(name: "Alice")
+      time = "2023-06-20 21:59:59"
+      user.sleeps.create!(clock_in: time)
+  
+      user.clock_in(time)
+  
+      expect(user.errors.size).not_to eq(0)
+      expect(user.sleeps.size).to eq(1)
+      expect(user.errors.full_messages).to include("Cannot clock in if there is a clock in without clock out")
+    end
   end
 
-  it "can do clock out" do
-    user = User.create!(name: "Alice")
-    user.clock_in("2023-06-20 20:59:59")
+  context "clock out" do
+    it "can do clock out" do
+      user = User.create!(name: "Alice")
+      user.clock_in("2023-06-20 20:59:59")
 
-    time = "2023-06-20 21:59:59"
+      time = "2023-06-20 21:59:59"
 
-    user.clock_out(time)
+      user.clock_out(time)
 
-    expect(user.sleeps[0].clock_out.strftime("%F %T")).to eq(time)
-    expect(user.sleeps[0].duration_in_second).to eq(3600)
+      expect(user.sleeps[0].clock_out.strftime("%F %T")).to eq(time)
+      expect(user.sleeps[0].duration_in_second).to eq(3600)
+    end
+
+    it "cannot do clock out if there is no previous check in" do
+      user = User.create!(name: "Alice")
+
+      time = "2023-06-20 21:59:59"
+
+      user.clock_out(time)
+
+      expect(user.errors.size).not_to eq(0)
+      expect(user.sleeps.last).to eq(nil)
+      expect(user.errors.full_messages).to include("Cannot clock out without previous clock in")
+    end
+  end
+  
+  context "follow" do
+    it "can follow another user" do
+      user1 = User.create!(name: "Alice")
+      user2 = User.create!(name: "Bob")
+
+      user1.follow(user2)
+
+      user1.reload
+      expect(user1.followings.size).to eq(1)
+      expect(user1.followings[0].id).to eq(user2.id)
+    end
   end
 
-  it "cannot do clock out if there is no previous check in" do
-    user = User.create!(name: "Alice")
+  context "unfollow" do
+    it "can unfollow another user" do
+      user1 = User.create!(name: "Alice")
+      user2 = User.create!(name: "Bob")
 
-    time = "2023-06-20 21:59:59"
+      user1.follow(user2)
+      user1.unfollow(user2)
 
-    user.clock_out(time)
+      user1.reload
+      expect(user1.followings.size).to eq(0)
+    end
 
-    expect(user.errors.size).not_to eq(0)
-    expect(user.sleeps.last).to eq(nil)
-    expect(user.errors.full_messages).to include("Cannot clock out without previous clock in")
-  end
+    # it "test" do
+    #   user1 = User.create!(name: "Alice")
+    #   user2 = User.create!(name: "Bob")
 
-  it "cannot do clock in if there is a clock in without clock out" do
-    user = User.create!(name: "Alice")
-    time = "2023-06-20 21:59:59"
-    user.sleeps.create!(clock_in: time)
+    #   user1.unfollow(user2)
 
-    user.clock_in(time)
-
-    expect(user.errors.size).not_to eq(0)
-    expect(user.sleeps.size).to eq(1)
-    expect(user.errors.full_messages).to include("Cannot clock in if there is a clock in without clock out")
-  end
-
-  it "can follow another user" do
-    user1 = User.create!(name: "Alice")
-    user2 = User.create!(name: "Bob")
-
-    user1.follow(user2)
-
-    user1.reload
-    expect(user1.followings.size).to eq(1)
-    expect(user1.followings[0].id).to eq(user2.id)
-  end
-
-  it "can unfollow another user" do
-    user1 = User.create!(name: "Alice")
-    user2 = User.create!(name: "Bob")
-
-    user1.follow(user2)
-    user1.unfollow(user2)
-
-    user1.reload
-    expect(user1.followings.size).to eq(0)
+    #   user1.reload
+    #   expect(user1.followings.size).to eq(0)
+    # end
   end
 end
