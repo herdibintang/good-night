b457ad5383b8032718b460081538b118f0d23372
Herdi Bintang <herdibintang@gmail.com>
2023-06-12 12:23:23 +0700

    another routes clean up

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     member do
       post :clock_in, path: "clock-in"
       post :clock_out, path: "clock-out"
-      post :follow, path: "follow"
-      post :unfollow, path: "unfollow"
-      get :sleeps, path: "sleeps"
+      post :follow
+      post :unfollow
+      get :sleeps
       get "followings/sleeps", to: "users#followings_sleeps"
     end
   end
