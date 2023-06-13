5675246da9a1f58d7cd8ab1a2276f97c0c1d5ca5
Herdi Bintang <herdibintang@gmail.com>
2023-06-13 11:41:00 +0700

    clean up controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,117 +25,105 @@ class UsersController < ApplicationController
   end
 
   def clock_in
-    begin
-      user = User.find(params[:id]).clock_in(params.require(:datetime))
+    user = User.find(params[:id]).clock_in(params.require(:datetime))
 
-      render json: { message: "Clock in success" }
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :conflict
-    end
+    render json: { message: "Clock in success" }
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :conflict
   end
 
   def clock_out
-    begin
-      user = User.find(params[:id]).clock_out(params.require(:datetime))
+    user = User.find(params[:id]).clock_out(params.require(:datetime))
 
-      render json: { message: "Clock out success" }
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :conflict
-    end
+    render json: { message: "Clock out success" }
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :conflict
   end
 
   def follow
-    begin
-      user = User.find(params[:id]).follow(User.find(params.require(:user_id)))
+    user = User.find(params[:id]).follow(User.find(params.require(:user_id)))
 
-      render json: { message: "Follow success" }
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :conflict
-    end
+    render json: { message: "Follow success" }
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :conflict
   end
 
   def unfollow
-    begin
-      user = User.find(params[:id])
-      
-      if !user.followings.include?(User.find(params.require(:user_id)))
-        return render json: { error: "Not following this user" }, status: :conflict
-      end
-
-      user.unfollow(User.find(params.require(:user_id)))
-
-      render json: { message: "Unfollow success" }
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :conflict
+    user = User.find(params[:id])
+    
+    if !user.followings.include?(User.find(params.require(:user_id)))
+      return render json: { error: "Not following this user" }, status: :conflict
     end
+
+    user.unfollow(User.find(params.require(:user_id)))
+
+    render json: { message: "Unfollow success" }
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :conflict
   end
 
   def followings_sleeps
-    begin
-      data = Sleep.includes(:user)
-        .joins(user: :followed)
-        .where(follows: { from_user_id: params[:id] })
-        .where({ clock_in: Date.today.last_week.beginning_of_week..Date.today.last_week.at_end_of_week })
-        .order(duration_in_second: :desc)
-        .map { |sleep|
-          {
-            clock_in: sleep.clock_in.strftime("%F %T"),
-            clock_out: sleep.clock_out.try(:strftime, "%F %T"),
-            duration_in_second: sleep.duration_in_second,
-            user: {
-              name: sleep.user.name
-            }
+    data = Sleep.includes(:user)
+      .joins(user: :followed)
+      .where(follows: { from_user_id: params[:id] })
+      .where({ clock_in: Date.today.last_week.beginning_of_week..Date.today.last_week.at_end_of_week })
+      .order(duration_in_second: :desc)
+      .map { |sleep|
+        {
+          clock_in: sleep.clock_in.strftime("%F %T"),
+          clock_out: sleep.clock_out.try(:strftime, "%F %T"),
+          duration_in_second: sleep.duration_in_second,
+          user: {
+            name: sleep.user.name
           }
         }
+      }
 
-      render json: { data: data }
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :conflict
-    end
+    render json: { data: data }
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :conflict
   end
 
   def sleeps
-    begin
-      data = User.includes(:sleeps)
-        .find(params[:id])
-        .sleeps
-        .order(created_at: :desc)
-        .map { |sleep|
-          {
-            clock_in: sleep.clock_in.strftime("%F %T"),
-            clock_out: sleep.clock_out.try(:strftime, "%F %T"),
-            duration_in_second: sleep.duration_in_second
-          }
+    data = User.includes(:sleeps)
+      .find(params[:id])
+      .sleeps
+      .order(created_at: :desc)
+      .map { |sleep|
+        {
+          clock_in: sleep.clock_in.strftime("%F %T"),
+          clock_out: sleep.clock_out.try(:strftime, "%F %T"),
+          duration_in_second: sleep.duration_in_second
         }
+      }
 
-      render json: { data: data }
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :conflict
-    end
+    render json: { data: data }
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :conflict
   end
 
   def followings
