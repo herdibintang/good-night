526c857214d12cd43abe7114e69a379d0c1ae786
Herdi Bintang <herdibintang@gmail.com>
2023-06-19 15:11:30 +0700

    SleepEntity can end

--- spec/entities/sleep_entity_spec.rb
+++ spec/entities/sleep_entity_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
+# require_relative '../app/entities/user_entity'
+# require_relative '../app/entities/sleep_entity'
 
 describe SleepEntity do
-  it 'can set start_at' do
+  it 'can start' do
     datetime = "2023-06-20 21:00:00"
 
     sleep_entity = SleepEntity.new(start_at: datetime)
     
     expect(sleep_entity.start_at).to eq(datetime)
+  end
+
+  it 'can end' do
+    datetime = "2023-06-20 21:00:00"
+
+    sleep_entity = SleepEntity.new
+    sleep_entity.end_at = datetime
+    
+    expect(sleep_entity.end_at).to eq(datetime)
   end
 end
