95b347951dd8c3a74fe278099640ebc659c58e85
Herdi Bintang <herdibintang@gmail.com>
2023-06-20 15:46:16 +0700

    forgot to commit some code

--- app/entities/sleep_entity.rb
+++ app/entities/sleep_entity.rb
@@ -1,5 +1,9 @@
 class SleepEntity
   include ActiveModel::Validations
+  include ActiveModel::Dirty
+  
+  attr_accessor :id, :start_at, :end_at
+  define_attribute_methods :end_at
   
   attr_accessor :start_at, :end_at
   
@@ -9,5 +13,10 @@ class SleepEntity
 
   def ongoing?
     @end_at.nil?
+  end
+
+  def end_at=(value)
+    end_at_will_change!
+    @end_at = value
   end
 end
