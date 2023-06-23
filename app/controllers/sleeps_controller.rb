2220905bf2fbed546ea3451d3a0816beaab8c21e
Herdi Bintang <herdibintang@gmail.com>
2023-06-23 16:47:39 +0700

    move sleeps list response to jb

--- app/controllers/sleeps_controller.rb
+++ app/controllers/sleeps_controller.rb
@@ -1,16 +1,5 @@
 class SleepsController < ApplicationController
   def index
-    data = Sleep.includes(:user).order(created_at: :desc).map { |sleep|
-      {
-        start_at: sleep.start_at.strftime("%F %T"),
-        end_at: sleep.end_at.try(:strftime, "%F %T"),
-        duration_in_second: sleep.duration_in_second,
-        user: {
-          name: sleep.user.name
-        }
-      }
-    }
-
-    render json: { data: data }
+    @sleeps = Sleep.includes(:user).order(created_at: :desc)
   end
 end
