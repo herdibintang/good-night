342ffbb293a01ffd81bf1ecc44a3b2b534ff815a
Herdi Bintang <herdibintang@gmail.com>
2023-06-30 15:50:26 +0700

    move test POST /users/create to new file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,38 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "/users", type: :request do
-  describe "POST /create" do
-    it "can create user" do
-      params = {
-        name: "Alice"
-      }
-
-      post "/users", params: params, as: :json
-
-      expect(response).to have_http_status(:ok)
-
-      body = JSON.parse(response.body)
-      expect(body["message"]).to eq("Create user success")
-      expect(body["data"]["id"]).not_to eq(nil)
-      expect(body["data"]["name"]).to eq("Alice")
-
-      expect(User.last.name).to eq("Alice")
-    end
-
-    it "return 400 when name is empty" do
-      params = {
-        name: ""
-      }
-
-      post "/users", params: params, as: :json
-
-      expect(response).to have_http_status(:bad_request)
-
-      body = JSON.parse(response.body)
-      expect(body["error"]).to eq("param is missing or the value is empty: name")
-    end
-  end
-
   describe "POST /index" do
     it "show user list" do
       User.create!(name: "Alice")
