0efbdfc1778d041620471ae243822dddf7dbcf06
Herdi Bintang <herdibintang@gmail.com>
2023-06-30 17:17:29 +0700

    move GET /users/followings test to a file

---
+++ spec/requests/users/followings_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "GET /users/followings", type: :request do
+  it "show user's followings" do
+    user1 = User.create!(name: "Alice")
+    user2 = User.create!(name: "John")
+
+    user1.follow(user2)
+
+    get "/users/#{user1.id}/followings", as: :json
+
+    expect(response).to have_http_status(:ok)
+
+    data = JSON.parse(response.body)["data"]
+    expect(data[0]["name"]).to eq("John")
+    expect(data[0]["created_at"]).to eq(nil)
+    expect(data[0]["updated_at"]).to eq(nil)
+  end
+end
