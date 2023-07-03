95ad654fc8248bf5d67d1c6b7408c251008af981
Herdi Bintang <herdibintang@gmail.com>
2023-07-03 17:04:56 +0700

    use add_sleep_from_hash in use case

--- app/use_cases/user_end_sleep_use_case.rb
+++ app/use_cases/user_end_sleep_use_case.rb
@@ -7,12 +7,7 @@ class UserEndSleepUseCase
     user_entity = UserEntity.new
     
     SleepGateway.find_all_by_user_id(context.user_id).each do |sleep|
-      sleep_entity = SleepEntity.new
-      sleep_entity.id = sleep[:id]
-      sleep_entity.start_at = sleep[:start_at]
-      sleep_entity.end_at = sleep[:end_at]
-      
-      user_entity.sleeps << sleep_entity
+      user_entity.add_sleep_from_hash(sleep)
     end
     
     user_entity.end_sleep(context.datetime)
