1eca1adeb3d247f4137859dec4c2ceafa0c211e7
Herdi Bintang <herdibintang@gmail.com>
2023-07-06 09:19:02 +0700

    refactor test to stub sleep gateway for UserEndSleepUseCase

--- spec/use_cases/user_end_sleep_use_case_spec.rb
+++ spec/use_cases/user_end_sleep_use_case_spec.rb
@@ -2,34 +2,32 @@ require 'rails_helper'
 
 describe UserEndSleepUseCase do
   it 'success' do
-    user = User.create!(name: "Alice")
-
-    Sleep.insert_all([
-      {
-        user_id: user.id,
-        start_at: "2023-06-10 20:00:00",
-        end_at: "2023-06-10 21:00:00"
-      },
-      {
-        user_id: user.id,
-        start_at: "2023-06-20 20:59:59",
-        end_at: nil
-      }
-    ])
+    sleep_gateway = double("sleep_gateway")
+    allow(sleep_gateway).to receive(:find_all_by_user_id) {
+      [
+        {
+          start_at: DateTime.parse("2023-06-10 20:00:00"),
+          end_at: DateTime.parse("2023-06-10 21:00:00"),
+          duration_in_second: 3600
+        },
+        {
+          start_at: DateTime.parse("2023-06-20 20:59:59"),
+          end_at: nil,
+          duration_in_second: nil
+        }
+      ]
+    }
+    allow(sleep_gateway).to receive(:update)
 
     datetime = "2023-06-20 21:59:59"
 
     result = UserEndSleepUseCase.call(
-      sleep_gateway: SleepGateway,
-      user_id: user.id,
+      sleep_gateway: sleep_gateway,
+      user_id: 1,
       datetime: datetime
     )
-    
+
     expect(result.success?).to eq(true)
-    expect(user.sleeps[0].start_at.strftime("%F %T")).to eq("2023-06-10 20:00:00")
-    expect(user.sleeps[0].end_at.strftime("%F %T")).to eq("2023-06-10 21:00:00")
-    expect(user.sleeps[1].end_at.strftime("%F %T")).to eq(datetime)
-    expect(user.sleeps[1].duration_in_second).to eq(3600)
   end
 
   it 'cannot end sleep if datetime is before start sleep' do
