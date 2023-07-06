62aee0220611409e71a53c0667f98a768b9cfa36
Herdi Bintang <herdibintang@gmail.com>
2023-07-06 09:24:18 +0700

    refactor test to stub sleep gateway  for UserEndSleepUseCase spec in negative case

--- spec/use_cases/user_end_sleep_use_case_spec.rb
+++ spec/use_cases/user_end_sleep_use_case_spec.rb
@@ -31,21 +31,22 @@ describe UserEndSleepUseCase do
   end
 
   it 'cannot end sleep if datetime is before start sleep' do
-    user = User.create!(name: "Alice")
-
-    Sleep.insert_all([
-      {
-        user_id: user.id,
-        start_at: "2023-06-20 20:59:59",
-        end_at: nil
-      }
-    ])
+    sleep_gateway = double("sleep_gateway")
+    allow(sleep_gateway).to receive(:find_all_by_user_id) {
+      [
+        {
+          start_at: DateTime.parse("2023-06-10 20:00:00"),
+          end_at: DateTime.parse("2023-06-10 21:00:00"),
+          duration_in_second: 3600
+        }
+      ]
+    }
 
     datetime = "2023-06-10 21:59:59"
 
     result = UserEndSleepUseCase.call(
-      sleep_gateway: SleepGateway,
-      user_id: user.id,
+      sleep_gateway: sleep_gateway,
+      user_id: 1,
       datetime: datetime
     )
     
